1fd3ee48a3b001b6ccb09f8683ee56596f3f4d5e
Mitchell Hay <haym8@students.rowan.edu>
2017-09-27 12:16:23 -0400

    Simple blink project code

---
+++ Simple Blink/MSP430G2553/Debug/simple_blink.d
@@ -0,0 +1,13 @@
+# FIXED
+
+simple_blink.obj: ../simple_blink.c
+simple_blink.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+simple_blink.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+simple_blink.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics.h
+simple_blink.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics_legacy_undefs.h
+
+../simple_blink.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics_legacy_undefs.h: 
---
+++ Simple Blink/MSP430G2553/Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+../simple_blink.c 
+
+OBJS += \
+./simple_blink.obj 
+
+C_DEPS += \
+./simple_blink.d 
+
+C_DEPS__QUOTED += \
+"simple_blink.d" 
+
+OBJS__QUOTED += \
+"simple_blink.obj" 
+
+C_SRCS__QUOTED += \
+"../simple_blink.c" 
+
+
