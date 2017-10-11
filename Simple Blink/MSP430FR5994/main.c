631e9082d8b7c9f2378177dcf617c134485e3dbd
haym8 <31711187+haym8@users.noreply.github.com>
2017-10-11 19:58:56 -0400

    Simple blink FR5529 final

--- Simple Blink/MSP430FR5994/main.c
+++ Simple Blink/MSP430FR5994/main.c
@@ -1,6 +1,14 @@
+/*
+ * Mitchell Hay
+ * RU09342
+ * Lab 2 Simple Blink
+ * MSP430FR5594
+ */
+
 #include <msp430.h>
 
 int main(void) {
+  // Set up counter
   volatile int i;
 
   // stop watchdog timer
@@ -9,16 +17,16 @@ int main(void) {
   // Disable the GPIO power-on default high-impedance mode
   PM5CTL0 &= ~LOCKLPM5;
 
-  // set up bit 0 of P1 as output
+  // Set up bit 0 of P1 as output
   P1DIR = BIT0;
-  // intialize bit 0 of P1 to 0
+  // Intialize bit 0 of P1 to 0
   P1OUT = BIT0;
 
   // loop forever
   for (;;) {
-    // toggle bit 0 of P1
+    // Toggle bit 0 of P1
     P1OUT ^= BIT0;
-    // delay for a while
+    // Delay for a while
     for (i = 0; i < 30000; i++);
   }
 }
