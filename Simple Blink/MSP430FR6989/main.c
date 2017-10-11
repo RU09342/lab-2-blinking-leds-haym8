edfad313c885fb7c821755aab5f1dfd1c49ca6d0
haym8 <31711187+haym8@users.noreply.github.com>
2017-10-11 19:59:59 -0400

    Simple blink FR6989 final

--- Simple Blink/MSP430FR6989/main.c
+++ Simple Blink/MSP430FR6989/main.c
@@ -16,16 +16,16 @@ int main(void) {
   // Disable the GPIO power-on default high-impedance mode
   PM5CTL0 &= ~LOCKLPM5;
 
-  // set up bit 0 of P1 as output
+  // Set up bit 0 of P1 as output
   P1DIR = BIT0;
-  // intialize bit 0 of P1 to 0
+  // Intialize bit 0 of P1 to 0
   P1OUT = BIT0;
 
   // loop forever
   for (;;) {
-    // toggle bit 0 of P1
+    // Toggle bit 0 of P1
     P1OUT ^= BIT0;
-    // delay for a while
+    // Delay for a while
     for (i = 0; i < 30000; i++);
   }
 }
