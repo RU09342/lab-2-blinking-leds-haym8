0a4185c81127c4cdc532b69a74b66b094574ba1b
haym8 <31711187+haym8@users.noreply.github.com>
2017-10-11 19:51:31 -0400

    Simple blink F5529 final

--- Simple Blink/MSP430F5529/main.c
+++ Simple Blink/MSP430F5529/main.c
@@ -8,20 +8,21 @@
 #include <msp430.h>
 
 int main(void) {
+  // Set up counter
   volatile int i;
 
   // stop watchdog timer
   WDTCTL = WDTPW | WDTHOLD;
-  // set up bit 0 of P1 as output
+  // Set up bit 0 of P1 as output
   P1DIR = BIT0;
-  // intialize bit 0 of P1 to 0
-  P1OUT = 0x00;
+  // Intialize bit 0 of P1 to 0
+  P1OUT = BIT0;
 
   // loop forever
   for (;;) {
-    // toggle bit 0 of P1
+    // Toggle bit 0 of P1
     P1OUT ^= BIT0;
-    // delay for a while
+    // Delay for a while
     for (i = 0; i < 30000; i++);
   }
 }
