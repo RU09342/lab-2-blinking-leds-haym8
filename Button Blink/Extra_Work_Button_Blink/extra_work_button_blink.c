8642b46751d022e671f9b0e699b5d815e571a410
haym8 <31711187+haym8@users.noreply.github.com>
2017-10-11 20:23:52 -0400

    Button blink extra work final

--- Button Blink/Extra_Work_Button_Blink/extra_work_button_blink.c
+++ Button Blink/Extra_Work_Button_Blink/extra_work_button_blink.c
@@ -3,8 +3,9 @@
  * RU09342
  * Lab 2 Button Blink Extra Work
  * MSP430G2553
+*/
 
-#include  <msp430g2553.h> // For the M430G2553 chip,
+#include  <msp430g2553.h>
 
 void main(void)
 {
@@ -25,42 +26,47 @@ void main(void)
 	// Enable pull up resistor
 	P1REN |= BIT3;
 
+	// Set up button press counter
 	int j = 0;
 	while (1)
 	{
+		// Use i for delay counter
 		volatile int i;
 		// When button is pressed, change speed
-	    while ((P1IN & BIT3) != BIT3)
-	    {
-	    	j++;
-	    	for (i = 0; i < 0x6000; i++);
-
-	    }
-
-	    if (j == 1)
-	    {
-	    	while(1)
+	    	while ((P1IN & BIT3) != BIT3)
 	    	{
-	    		// toggle bit 0 of P1
-	    		P1OUT ^= 0x01;
-	    		// delay for a while
-	    		for (i = 0; i < 0x6000; i++);
-	    		if ((P1IN & BIT3) != BIT3)
+	    		j++;
+	    		for (i = 0; i < 0x6000; i++);	// Delay to account for debouncing
+
+	    	}
+
+	    	//
+	    	if (j == 1)
+	    	{
+	    		while(1)
 	    		{
-	    			P1OUT &= ~BIT0;
-	    			break;
+	    			// toggle bit 0 of P1
+	    			P1OUT ^= 0x01;
+	    			// delay for a while
+	    			for (i = 0; i < 0x6000; i++);
+				// Break out of while loop when button is pressed
+	    			if ((P1IN & BIT3) != BIT3)
+	    			{
+	    				P1OUT &= ~BIT0;
+	    				break;
+	    			}
 	    		}
 	    	}
-	    }
 
-	    if (j == 2)
-	    {
+	    	if (j == 2)
+	    	{
 	    	    	while(1)
 	    	    	{
 	    	    		// toggle bit 0 of P1
 	    	    		P1OUT ^= 0x01;
 	    	    		// delay for a while
 	    	    		for (i = 0; i < 0x3000; i++);
+				// Break out of while loop when button is pressed
 	    	    		if ((P1IN & BIT3) != BIT3)
 	    	    		{
 	    	    			P1OUT &= ~BIT0;
@@ -71,19 +77,23 @@ void main(void)
 
 	    if (j == 3)
 	    {
-	      	    	while(1)
-	      	    	{
-	      	    		// toggle bit 0 of P1
-	    	    	    		P1OUT ^= 0x01;
-	    	    	    		// delay for a while
-	    	    	    		for (i = 0; i < 0x1500; i++);
-	    	    	    		if ((P1IN & BIT3) != BIT3)
-	    	    	    		{
-	    	    	    			P1OUT &= ~BIT0;
-	    	    	    			break;
-	    	    	    		}
-	    	    	    	}
+	    	while(1)
+	      	{
+	      		// toggle bit 0 of P1
+	    	    	P1OUT ^= 0x01;
+	    	    	// delay for a while
+	    	    	for (i = 0; i < 0x1500; i++);
+			// Break out of while loop when button is pressed
+	    	    	if ((P1IN & BIT3) != BIT3)
+	    	    	{
+				// LED turned off
+	    	    		P1OUT &= ~BIT0;
+	    	    	    	break;
+	    	    	}
+	    	}
 	    }
+		
+	    // Reset button counter after highest speed
 	    else if (j > 3)
 	    {
 	    	j -= 4;
@@ -91,4 +101,3 @@ void main(void)
 
 	}
 }
-*/
