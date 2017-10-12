078dd5e6ea5f289ccd95d3782bd6efe2de03ad5e
haym8 <31711187+haym8@users.noreply.github.com>
2017-10-11 20:10:37 -0400

    Multiple blink FR6989 final

--- Multiple Blink/MSP430FR6989/main.c
+++ Multiple Blink/MSP430FR6989/main.c
@@ -10,28 +10,29 @@ int main(void) {
 	// Disable the GPIO power-on default high-impedance mode
 	PM5CTL0 &= ~LOCKLPM5;
 
-
-	// Set the directions of pins 1.0 and 4.7 (LED pins)
+	// Set the directions of pins 1.0 and 9.7 (LED pins)
 	P1DIR = BIT0;
 	P9DIR = BIT7;
 
-	// Set the red LED to on to start, stagger the blinking
-	P1OUT |= BIT0;
+	// Start both LEDs off
+	P1OUT &= ~BIT0;
 	P9OUT &= ~BIT7;
 
+	// Loop forever
 	while(1)
 	{
 		// Delay
 		i++;
 		j++;
 
-		// Toggle LEDs on or off
+		// Toggle LED1 on or off
 		if (i >= 10000)
 		{
 			P1OUT ^= BIT0;
 			i = 0;
 		}
 
+		// Toggle LED2 on or off, slower rate
 		if (j == 20000)
 		{
 			P9OUT ^= BIT7;
