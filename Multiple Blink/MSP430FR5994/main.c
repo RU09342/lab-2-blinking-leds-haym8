96b13d3c0ebfb371458737d70673eb7703653af9
haym8 <31711187+haym8@users.noreply.github.com>
2017-10-11 20:08:54 -0400

    Multiple blink FR5594 final

--- Multiple Blink/MSP430FR5994/main.c
+++ Multiple Blink/MSP430FR5994/main.c
@@ -1,6 +1,7 @@
 #include <msp430.h>
 
 int main(void) {
+	// Make counters
 	volatile int i = 0;
 	volatile int j = 0;
 
@@ -10,29 +11,30 @@ int main(void) {
 	// Disable the GPIO power-on default high-impedance mode
 	PM5CTL0 &= ~LOCKLPM5;
 
-
-	// Set the directions of pins 1.0 and 4.7 (LED pins)
+	// Set the directions of pins 1.0 and 1.1 (LED pins)
 	P1DIR |= BIT0;
 	P1DIR |= BIT1;
 
 	// Set both LEDs off
-	P1OUT |= BIT0;
+	P1OUT &= ~BIT0;
 	P1OUT &= ~BIT1;
 
+	// Loop forever
 	while(1)
 	{
 		// Delay
 		i++;
 		j++;
 
-		// Toggle LEDs on or off
+		// Toggle LED1 on or off
 		if (i >= 10000)
 		{
 			P1OUT ^= BIT0;
 			i = 0;
 		}
-
-		if (j == 20000)
+		
+		// Toggle LED1 on or off, slower rate
+		if (j >= 20000)
 		{
 			P1OUT ^= BIT1;
 			j = 0;
